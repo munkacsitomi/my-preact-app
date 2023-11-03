6b78dc75a7851e38117c23f0070bb024dc5421f5
Tamas Munkacsi <tomi.munkacsi@gmail.com>
2023-11-03 22:23:19 +0100

    Revert "add tailwind css"
    This reverts commit ad91fd222a01ddb9e8d73951eba68f9900f9fa03.

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
-import './tailwind.css';
+import './app.css'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-blue-500">Hello</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
     </>
   )
 }
